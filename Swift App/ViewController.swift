4431455207b32ac21c0fc4e22ce2656645deadb9
Thom Leiter <thom@40degreesmedia.com>
2017-03-04 01:29:22 -0500

    My first commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var pushCount = 0
+    
+    @IBAction func buttonPushed(_ sender: UIButton) {
+        pushCount = pushCount + 1
+        print(pushCount)
+        if pushCount == 10 {
+            theLabel.text = "Button pushed 10X!"
+            self.view.backgroundColor = UIColor.red
+        }
+        if pushCount > 10 {
+            self.view.backgroundColor = UIColor.white
+        }
+        if pushCount == 20 {
+            theLabel.text = "You made it to 20X!!!"
+            self.view.backgroundColor = UIColor.green
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
